85962a66832e8c1b9cf45cdaa458dc37cb7dde4c
Bill Harshbarger <bill.harshbarger@gmail.com>
2017-03-12 20:32:38 -0500

    harvester module

--- AutOSINT.py
+++ AutOSINT.py
@@ -41,6 +41,7 @@ try:
 	from googledork import Googledork
 	from shodansearch import Shodansearch
 	from pastebinscrape import Pastebinscrape
+	from theharvester import Theharvester
 
 except ImportError as e:
 	raise ImportError('Error importing %s' % e)
@@ -216,10 +217,14 @@ def main():
 	
 
 
-	
+
 	# call function if -t arg
 	if args.theharvester is True:
-		harvesterResult=the_harvester(args, lookup, reportDir)
+		
+		theHarvester=Theharvester()
+		theHarvester.run(args, lookup, reportDir)
+
+		#harvesterResult=the_harvester(args, lookup, reportDir)
 	
 	#call function if -c arg 
 	if args.creds is True:
@@ -254,57 +259,7 @@ def main():
 
 				
 #*******************************************************************************
-def the_harvester(args, lookup, reportDir):
 
-	#https://github.com/laramies/theHarvester
-	if args.theharvester is True:
-		
-		#init lists
-		harvested = []
-		harvesterGoogleResult = []
-		harvesterLinkedinResult = []
-		harvesterResult=[]
-
-		#based on domain or ip, enumerate with index and value
-		for i, l in enumerate(lookup):
-
-			#open file to write to
-			harvesterFile=open(reportDir+l+'/'+l+'_theharvester.txt','w')
-
-			#run harvester with -b google on lookup
-			try:
-				print '[+] Running theHarvester -b google -d %s ' % l
-				harvesterGoogleCmd = subprocess.Popen(['theharvester', '-b', 'google', '-d', str(l), '-l', '500', '-h'], stdout = subprocess.PIPE).communicate()[0].split('\r\n')
-			except:
-				print '[-] Error running theharvester. Make sure it is in your PATH and you are connected to the Internet'
-				harvesterResult.append('Error running theHarvester')
-				continue
-
-			#run harvester with -b linkedin on lookup
-			try:
-				print '[+] Running theHarvester -b linkedin -d %s ' % l
-				harvesterLinkedinCmd = subprocess.Popen(['theharvester', '-b', 'linkedin', '-d', str(l), '-l', '500', '-h'], stdout = subprocess.PIPE).communicate()[0].split('\r\n')
-			except:
-				print '[-] Error running theharvester. Make sure it is in your PATH and you are connected to the Internet'
-				harvesterResult.append('Error running theHarvester')
-				continue
-
-			#append lists together
-			harvesterResult.append(harvesterGoogleCmd)
-			harvesterResult.append(harvesterLinkedinCmd)
-
-			#append resutls and write to lookup result file
-			for r in harvesterResult:
-				harvesterFile.writelines(r)
-
-				
-		#verbosity
-		if args.verbose is True:
-			for h in harvesterResult: print '\n'.join(h)
-
-
-		#return list object
-		return harvesterResult
 
 
 
