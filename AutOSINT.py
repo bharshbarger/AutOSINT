4614b79bda9107461884070d60fe921e3af609d7
Bill Harshbarger <bill.harshbarger@gmail.com>
2017-03-12 19:48:36 -0500

    modularize hibp

--- AutOSINT.py
+++ AutOSINT.py
@@ -35,6 +35,7 @@ try:
 	from webscrape import Scraper
 	from whois import Whois
 	from dnsquery import Dnsquery
+	from hibp import Haveibeenpwned
 
 except ImportError as e:
 	raise ImportError('Error importing %s' % e)
@@ -187,9 +188,11 @@ def main():
 
 	#call function if -b arg
 	if args.hibp is True:
-		hibpResult = hibp_search(args, lookup, reportDir)
-	
 
+		hibpSearch = Haveibeenpwned()
+		hibpSearch.run(args, lookup, reportDir)
+		#hibpResult = hibp_search(args, lookup, reportDir)
+	
 
 	#call function if -g arg
 	if args.googledork is not None:
@@ -229,36 +232,6 @@ def main():
 
 	#run the docx report. text files happen in the respective functions
 	write_report(args, reportDir, lookup, whoisResult, dnsResult, googleResult, shodanResult, pasteScrapeResult, harvesterResult, scrapeResult, credResult, pyfocaResult)
-
-
-#*******************************************************************************
-#hibp api search to implement
-#https://haveibeenpwned.com/API/v2
-#https://haveibeenpwned.com/Pastes/Latest
-def hibp_search(args, lookup, reportDir):
-	userAgent = {'User-agent': 'Mozilla/5.0'}
-	if args.hibp is True:
-		for i,l in enumerate(lookup):
-			print '[+] Searching haveibeenpwned.com for %s' % l
-			scrapeFile=open(reportDir+l+'/'+l+'_haveibeenpwned.txt','w')
-			url = 'https://haveibeenpwned.com/api/v2/breachedaccount/test@example.com?domain=%s' % l
-
-
-			if args.verbose is True:print '[+] Searching haveibeenpwned.com for %s' % (l.split('.')[0])
-
-			#http://docs.python-guide.org/en/latest/scenarios/scrape/
-			try:
-				page = requests.get(url, headers = userAgent, verify=False)
-				#build html tree
-				page.json()
-				
-
-			except:
-				print '[-] Connection error or no result on ' + url +':'
-				print '[-] Status code %s' % page.status_code
-				continue
-
-		
 
 
 #*******************************************************************************
